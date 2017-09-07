3b56fbcb3b58dbbde01892509ed27e9deba21b7e
Swelly <Surfswell17@gmail.com>
2017-09-07 08:28:57 -0700

    overhaul w/ new boilerplate

--- app/constants/AppConstants.js
+++
@@ -1,5 +0,0 @@
-import pkg from '../../package.json';
-
-export const DEV = (process.env.NODE_ENV !== 'production');
-export const APP_TITLE = "Wesley Magness | Designer & Developer";
-export const NAVIGATION_UPDATED = 'NAVIGATION_UPDATED';
